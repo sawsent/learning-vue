89b926765a878d6951c9ed80ff284a3e75d579ad
Vicente Figueiredo <vicenteppfigueiredo@gmail.com>
2024-07-09 13:50:19 +0100

    Added ConditionalRendering to App

--- code/src/App.vue
+++ code/src/App.vue
@@ -3,12 +3,14 @@ import IterationTesting from './components/IterationTesting.vue';
 import ModelComponent from './components/ModelComponent.vue';
 import ComponentsComponent from './components/_01_Components/ComponentsComponent.vue';
 import EventsComponent from './components/_02_Events/EventsComponent.vue';
+import ConditionalRendering from './components/ConditionalRendering.vue';
 import { ref } from 'vue';
 
 let currentlyShowing = ref("Components");
 const myLearnedShit = [
 	"Components",
 	"Events",
+	"Conditional Rendering",
 	"Iteration",
 	"Model",
 	
@@ -25,6 +27,7 @@ const enable = (element) => currentlyShowing.value = element;
 	<ModelComponent v-if="currentlyShowing === 'Model'"/>
 	<ComponentsComponent v-if="currentlyShowing === 'Components'"/>
 	<EventsComponent v-if="currentlyShowing === 'Events'" />
+	<ConditionalRendering v-if="currentlyShowing === 'Conditional Rendering'" />
 </template>
 
 <style scoped>
---
+++ code/src/components/ConditionalRendering.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { ref } from 'vue';
+    let isVisible = ref(true);
+
+    const toggleVisibility = () => {
+        isVisible.value = !isVisible.value;
+        console.log(isVisible.value)
+    }
+</script>
+
+<template>
+    <p v-show="isVisible">This will show if thigerr</p>
+    <button @:click="toggleVisibility">toggle</button>
+</template>
+
